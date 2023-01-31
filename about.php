fa98cded773b9d56ac51f8c09a92f6667e16d51d
Redon <93224452+RedonBe@users.noreply.github.com>
2023-02-01 00:30:20 +0100

    Add files via upload
    new

---
+++ about.php
@@ -0,0 +1,99 @@
+<?php include_once "inc/header.php";?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" type="text/css" href="about.css">
+
+	<title>About</title>
+</head>
+
+	<body>
+	<div class="bar">
+		<div id="mm">
+			<p>Kurse profesionale në menaxhim nga një ekip profesionale të akredituar
+			në programim dhe në mësimdhënie të nivelit të lartë.
+			Të gjitha në një vend.Një ëndërr e themeluesit tonë që në fund u bë realitet.
+			Tani në ekipin tonë kemi 3 Mësimdhënës profesionalë që të gjithë më të mirët në fushën e tyre,
+			të përkushtuar dhe vetëm me një qëllim dhe ai është përmisimi i njohurive tek të rinjtë në fushen
+			e programimit.Mos harro,kurrë nuk është vonë të i'a fillosh programimit.!</p>
+
+		</div>
+	</div>
+		
+	<div class="section-header">
+			<h3>Our team</h3>
+
+	<div class="wrapper">
+
+	<div class="team-member">							
+		 <img src="images/leonard.jpg" >
+ 			<h3>Redon Begu </h3>
+    		<p>Full Stack Developer</p>
+
+    </div>
+
+	<div class="team-member ">
+		<img src="images/elsa.jpg" >
+		<h3>Elsa Topalli</h3>
+   			 <p>Full Stack Developer</p>
+   	</div>
+
+	   	<div class="team-member ">
+			<img src="images/partneri3.jpg" >
+			<h3>Ashley Connor</h3>
+	   	 		<p>Web Developer</p>
+	   	</div>
+  	
+</div>
+	</div>
+	<div class="teksti">
+		<div class="pershkrimi">
+			<p><b>Redon</b>:
+			Njëri prej themeluesit dhe njëkohësisht Mësimdhënës në programin tonë.Profesional,
+			me vullnet të mirë dhe me një korrektësi në punë e bën një prej më të mirëve në punen
+			e tij.Si një Full Stack Developer nëse ndjekni kursin e tij do të përfitoni njohuri
+			të mjaftueshme që të filloni rrugëtimin tuaj..</p>
+		</div>
+		<div class="pershkrimi">
+			<p><b>Elsa</b>:
+			Njëri prej themeluesit dhe njëkohësisht Mësimdhënës në programin tonë.Profesional,
+			me vullnet të mirë dhe me një korrektësi në punë e bën një prej më të mirëve në punen
+			e tij.Si një Full Stack Developer nëse ndjekni kursin e tij do të përfitoni njohuri
+			të mjaftueshme që të filloni rrugëtimin tuaj..</p>
+		</div>
+		<div class="pershkrimi ">
+			<p><b>Ashley</b>:
+			Së fundmi i është bashkuar ekipes tonë Ashley.Një kolege punëtore me qëllim mësimdhenien më të mirë
+		 	të mundshme.Puna e saj tregon shume pas përvojës së saj në tregun botëror dhe me cilësi të
+			jashtëzakonshme.Ndjekja e kursit të saj për Web Development do të përfitoni njohuri të mjaftueshme
+			që të filloni rrugëtimin tuaj..</p>
+		</div>
+	</div>
+
+	<div id="footer">
+		<h4>Follow us</h4>
+		<p>Mos harroni të na ndiqni përmes profileve të mediave tona sociale dhe vazhdoni me lajmet tona të fundit.</p>
+		<div class="imgimg">
+			<div class ="image">
+				
+				<a href="https://www.facebook.com/" target="_blank"><img src="https://img.icons8.com/color/48/000000/facebook.png"/> </a>
+
+			</div>
+			<div class="image">
+				<a href="https://www.instagram.com/"target="_blank"><img src="https://img.icons8.com/color/48/000000/instagram-new.png"/></a>
+				
+			</div>
+			<div class="image">
+				<a href="https://www.twitter.com/"target="_blank"><img src="images/twitter.png"></a>
+				
+			</div>
+
+		</div>
+		<p class="paragraf">techwebs&copy;2022</p>
+	</div>
+
+	
+</body>
+</html>
